bc40c01378fd5a6be4fe95020e7379b17a6356e3
Liam Tran <liam@email.com>
2026-03-03 22:29:06 +0700

    Add Project

---
+++ LearnSourceTree/LearnSourceTree/LearnSourceTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSourceTreeApp.swift
+//  LearnSourceTree
+//
+//  Created by Trần Chí Thiện on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LearnSourceTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
