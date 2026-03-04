a5bec781093cef529ba31c15b99003519dd59a53
Liam Tran <liam@email.com>
2026-03-05 00:41:19 +0700

    [Bai 1] Create file 2 and file 3

---
+++ LearnSourceTree/LearnSourceTree/Bai1_CreateCommit/File2.swift
@@ -0,0 +1,18 @@
+//
+//  File2.swift
+//  LearnSourceTree
+//
+//  Created by Trần Chí Thiện on 5/3/26.
+//
+
+import SwiftUI
+
+struct File2: View {
+    var body: some View {
+        Text("Hello Word Create file 2")
+    }
+}
+
+#Preview {
+    File2()
+}
